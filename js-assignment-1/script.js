3c01b8f02cc35a090b456d1796ccde05e2d010dd
Prathap <prathap.matheswaran@siemens-healthineers.com>
2023-03-02 14:41:08 +0530

    implemented IIFE concept in main script.js file

--- js-assignment-1/script.js
+++ js-assignment-1/script.js
@@ -60,13 +60,16 @@ WARNING!!! WARNING!!!
   //}
 //}
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+( function () {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-for (var i = 0; i < names.length ; i++) {
-  var firstLetter = names[i].charAt(0);
-  if(firstLetter.toLowerCase() == 'j'){
-    byeSpeaker.speak(names[i]);
-  }else{
-    helloSpeaker.speak(names[i]);
+  for (var i = 0; i < names.length ; i++) {
+    var firstLetter = names[i].charAt(0);
+    if(firstLetter.toLowerCase() == 'j'){
+      byeSpeaker.speak(names[i]);
+    }else{
+      helloSpeaker.speak(names[i]);
+    }
   }
-}
+})();
+
